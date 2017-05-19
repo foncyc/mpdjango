95841b49e9392d5fe3e5f18a5ec60209c7ef9332
Foncy <foncyc@gmail.com>
2017-05-19 20:36:46 +0800

    attempt 7

--- mp/settings.py
+++ mp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=%9fs(r8vv%k(2$rt461occh*7ze*6o$q+xl0gz!^5=h(1vmi3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
